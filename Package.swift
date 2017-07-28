710756da1ab7dcceb4bf091d6268fa2f0d267b76
Matheus Brasil <matheus.brasil10@gmail.com>
2017-07-28 01:39:13 -0300

    chore(package): add testing dependencies

--- Package.swift
+++ Package.swift
@@ -5,6 +5,8 @@ let package = Package(
     name: "XHubMiddleware",
     dependencies: [
             .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2),
-            .Package(url: "https://github.com/IBM-Swift/BlueCryptor.git", majorVersion: 0)
+            .Package(url: "https://github.com/IBM-Swift/BlueCryptor.git", majorVersion: 0),            
+            .Package(url: "https://github.com/Quick/Quick.git", majorVersion: 1, minor: 1),
+            .Package(url: "https://github.com/Quick/Nimble.git", majorVersion: 7)
     ]
 )
