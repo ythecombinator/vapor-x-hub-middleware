3376146efdc09dbd43933b1dfc151f4a68f6ba60
Matheus Brasil <matheus.brasil10@gmail.com>
2017-07-30 15:03:04 -0300

    refactor(package data): build testing deps only if needed

--- Package.swift
+++ Package.swift
@@ -1,12 +1,24 @@
 import PackageDescription
 import Foundation
 
+var isSwiftPackagerManagerTestEnvironment: Bool {
+    return ProcessInfo.processInfo.environment["SPM_ENV"] == "test"
+}
+
 let package = Package(
     name: "XHubMiddleware",
-    dependencies: [
+    dependencies: {
+        var deps: [Package.Dependency] = [
             .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2),
-            .Package(url: "https://github.com/IBM-Swift/BlueCryptor.git", majorVersion: 0),            
-            .Package(url: "https://github.com/Quick/Quick.git", majorVersion: 1, minor: 1),
-            .Package(url: "https://github.com/Quick/Nimble.git", majorVersion: 7)
-    ]
+            .Package(url: "https://github.com/IBM-Swift/BlueCryptor.git", majorVersion: 0),
+        ]
+        if isSwiftPackagerManagerTestEnvironment {
+            deps += [
+                .Package(url: "https://github.com/Quick/Quick.git", majorVersion: 1, minor: 1),
+                .Package(url: "https://github.com/Quick/Nimble.git", majorVersion: 7)
+            ]
+        }
+        return deps
+    }(),
+    swiftLanguageVersions: [3]
 )
